aa64ec5a83f867e4e58d090d921d9974b263da0d
cwen0708 <cwen0708@gmail.com>
2016-11-28 19:22:37 +0800

    樣式上傳工具更新

---
+++ themes/tools/a.bat
@@ -0,0 +1,3 @@
+setup.py py2exe
+copy "C:\Dropbox\VMoon\argeweb-framework\themes\tools\dist\upload.exe" "C:\Dropbox\VMoon\argeweb-framework\themes\default\upload.exe" /Y
+C:\Dropbox\VMoon\argeweb-framework\themes\default\upload.exe
---
+++ themes/tools/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created with YooLiang Technology (侑良科技).
+# Author: Qi-Liang Wen (温啓良）
+# Web: http://www.yooliang.com/
+# Date: 2016/11/26
+
+
+from distutils.core import setup
+import py2exe
+import sys
+import certifi
+
+print certifi.where()
+sys.argv.append('py2exe')
+setup(
+    options={'py2exe': dict(bundle_files=1, optimize=2)},
+    console=["upload.py"],
+    data_files=["cacert.pem"],
+    zipfile=None
+)
