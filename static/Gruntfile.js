6d67842fc0f9bd554cc213b8db5503def3203774
cwen0708 <cwen0708@gmail.com>
2016-12-20 05:39:14 +0800

    修正錯誤的 copy 方式

--- static/Gruntfile.js
+++ static/Gruntfile.js
@@ -1,15 +1,32 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     var fs = require('fs');
-    function getAllFiles(root, css_rule, js_rule, ignore_font) {
+    function getFilesRule(css_rule, js_rule, use_uglify, use_cssmin, ignore_font) {
+        var root = __dirname + "/bower_components";
+        use_uglify = use_uglify || false;
+        use_cssmin = use_cssmin || false;
         ignore_font = ignore_font || false;
         var res = [], files = fs.readdirSync(root);
         files.forEach(function (file) {
             var pathname = root + '/' + file
                 , stat = fs.lstatSync(pathname);
             if (stat.isDirectory()) {
-                res.push({expand: true, cwd: 'bower_components/' + file, src: css_rule, dest: 'vendor/' + file, flatten: true, filter: 'isFile'});
-                res.push({expand: true, cwd: 'bower_components/' + file, src: js_rule, dest: 'vendor/' + file, flatten: true, filter: 'isFile'});
+                if (css_rule.length > 0){
+                    if (use_cssmin == false){
+                        res.push({expand: true, cwd: 'bower_components/' + file, src: css_rule, dest: 'vendor/' + file, flatten: true, filter: 'isFile'});
+                    } else {
+                        res.push({expand: true, cwd: 'bower_components/' + file, src: css_rule, dest: 'vendor/' + file, flatten: true, filter: 'isFile', ext: '.min.css'});
+                    }
+                }
+                if (js_rule.length > 0){
+                    if (use_uglify == false){
+                        res.push({expand: true, cwd: 'bower_components/' + file, src: js_rule, dest: 'vendor/' + file, flatten: true, filter: 'isFile'});
+                    } else {
+                        res.push({expand: true, cwd: 'bower_components/' + file, src: js_rule, dest: 'vendor/' + file, flatten: true, filter: 'isFile', ext: '.min.js'});
+                    }
+                }
                 if (ignore_font === false){
                     res.push({expand: true, src: ['bower_components/' + file + '**/fonts/**'], dest: 'vendor/fonts', flatten: true, filter: 'isFile'});
                 }
@@ -18,39 +35,66 @@ module.exports = function (grunt) {
         return res
     }
 
-    var css_rule = [
-        '**/jquery.steps.css',
-        '**/codemirror.css',
-        '**/*.min.css',
-        '**/*.min.css.map'
-    ];
-    var js_rule = [
-        '**/brace-fold.js',
-        '**/codemirror.js',
-        '**/css.js',
-        '**/htmlmixed.js',
-        '**/javascript.js',
-        '**/message*.js',
-        '**/keymaster.js',
-        '**/jquery.min.map',
-        '**/*.min.js',
-        '**/*.min.js.map'
-    ];
-    var files_rule = getAllFiles(__dirname + "/bower_components", css_rule, js_rule);
-    var files_rule_with_prism = getAllFiles(__dirname + "/bower_components", ['**/prism*.css'], ['**/prism*.js'], true);
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         copy: {
             main: {
-                files: files_rule
+                files: getFilesRule(
+                    [
+                        '**/*.min.css',
+                        '**/*.min.css.map'
+                    ], [
+                        '**/jquery.blueimp-gallery.min.js',
+                        '**/jquery.min.map',
+                        '**/*.min.js',
+                        '**/*.min.js.map'
+                    ]
+                )
             },
             prism: {
-                files: files_rule_with_prism
+                files: getFilesRule(['**/prism*.css'], ['**/prism*.js'], false, false, true)
+            }
+        },
+        uglify: {
+            main: {
+                options: {
+                    // 設定壓縮後檔頭要插入的註解
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                    // 使用 SourceMap 並且將 JS Source 與 Map 檔案放在一起
+                    sourceMap: true,
+                    sourceMapIncludeSources: true
+                },
+                files: getFilesRule([],
+                    [
+                        '**/brace-fold.js',
+                        '**/codemirror.js',
+                        '**/mode/css/css.js',
+                        '**/mode/htmlmixed/htmlmixed.js',
+                        '**/mode/javascript/javascript.js',
+                        '**/message*.js',
+                        '**/keymaster.js',
+                        '**/instantclick.js',
+                        '!*.min.js'
+                    ]
+                    , true, false, true)
+            }
+        },
+        cssmin: {
+            minify: {
+                files: getFilesRule(
+                    [
+                        '**/jquery.steps.css',
+                        '**/codemirror.css',
+                        '!*.min.css'
+                    ], [], false, true, true
+                )
             }
         }
     });
 
+    grunt.registerTask('c', ['copy:main']);
     grunt.registerTask('prism', ['copy:prism']);
-    grunt.registerTask('default', ['copy:main']);
+    grunt.registerTask('js', ['uglify:main']);
+    grunt.registerTask('css', ['cssmin:minify']);
+    grunt.registerTask('default', ['uglify:main', 'cssmin:minify', 'copy:main']);
 };
