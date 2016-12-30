29592dd51630bf2ce46c0125f6913063aa70a891
cwen0708 <cwen0708@gmail.com>
2016-12-30 15:35:00 +0800

    去除不重要的

--- argeweb/manage/update.py
+++ argeweb/manage/update.py
@@ -17,15 +17,15 @@ def main():
         os.chdir(dir)
         run('bower update -f')
 
-        # target_dir = os.path.join(base_dir, 'plugins')
-        # os.chdir(target_dir)
-        # run('bower update')
-        # run('bower list --paths --json > bower_path.json')
-
-        target_dir = os.path.join(base_dir, 'static')
+        target_dir = os.path.join(base_dir, 'plugins')
         os.chdir(target_dir)
         run('bower update')
         run('bower list --paths --json > bower_path.json')
+
+        # target_dir = os.path.join(base_dir, 'static')
+        # os.chdir(target_dir)
+        # run('bower update')
+        # run('bower list --paths --json > bower_path.json')
     else:
         for n in xrange(0, len(sys.argv)):
             arg = str(sys.argv[n])
