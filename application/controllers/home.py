49ef3a776ea2e562f191508157a0c8cefb07ac47
cwen0708 <cwen0708@gmail.com>
2017-05-31 23:31:14 +0800

    移至 zz last path

--- application/controllers/home.py
+++ application/controllers/home.py
@@ -17,17 +17,3 @@ class Home(Controller):
         title = u'前台'
         components = (scaffold.Scaffolding, Pagination, Search)
 
-    @route_with('/')
-    @route_with('/index.html')
-    @add_authorizations(auth.check_user)
-    def index(self):
-        # 取消樣版系統的快取
-        self.meta.view.cache = False
-        # 先從 Datastore 讀取樣版, 再從 實體檔案 讀取樣版 (template, themes 相關目錄)
-        self.meta.view.template_name = [u'assets:/themes/%s/index.html' % self.theme, u'/index.html']
-        if self.theme == 'default':
-            # 若有語系參數的話 ( hl )
-            index_path = u'%s.html' % self.params.get_string('hl', u'index').lower().replace('-', '')
-            self.meta.view.template_name = [u'assets:/themes/%s/%s' % (self.theme, index_path), index_path]
-        self.context['information'] = self.host_information
-
