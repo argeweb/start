9f63754b988db5eb5e40dcc3a08d63e3baa32162
cwen0708 <cwen0708@gmail.com>
2017-06-06 17:06:17 +0800

    修正實例快取時，造成 Model 緩存的問題
    強制使用中間層來避免過度的快取

--- argeweb/application.py
+++ argeweb/application.py
@@ -6,8 +6,10 @@
 # Web: http://www.yooliang.com/
 # Date: 2016/6/14.
 import os
-import logging
+from google.appengine.ext.appstats import recording
+
 debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
+version = os.environ['CURRENT_VERSION_ID']
 use_error_handlers = True
 _instance = None
 _setting = None
@@ -27,13 +29,11 @@ def get_instance():
     from argeweb.core import errors
     from argeweb.core import routing
     settings, s = get_setting()
- 
     appstats_settings = s['appstats']
     app_config_settings = s['app_config']
- 
+    debug = True
     _instance = WSGIApplication(
         debug=debug, config=app_config_settings)
- 
     # Custom Error Handlers
     if debug is False:
         _instance.error_handlers[400] = errors.handle_400
@@ -41,12 +41,8 @@ def get_instance():
         _instance.error_handlers[403] = errors.handle_403
         _instance.error_handlers[404] = errors.handle_404
         _instance.error_handlers[500] = errors.handle_500
+    routing.auto_route(_instance.router, debug, version)
 
-    routing.auto_route(_instance.router, debug)
- 
-    if (appstats_settings.get('enabled', False) and debug) or appstats_settings.get('enabled_live', True):
-        from google.appengine.ext.appstats import recording
-        _instance = recording.appstats_wsgi_middleware(_instance)
-    return _instance
+    return recording.appstats_wsgi_middleware(_instance)
  
 instance = get_instance()
