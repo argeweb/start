c61ea2fc704d1d7d82ec88e816725bba5dd61b01
cwen0708 <cwen0708@gmail.com>
2018-04-18 08:31:47 +0800

    wtforms 移入 core

--- argeweb/libs/wtforms/fields/__init__.py
+++
@@ -1,7 +0,0 @@
-from .core import *
-
-from .simple import *
-
-# Compatibility imports
-from .core import Label, Field, SelectFieldBase, Flags
-from ..utils import unset_value as _unset_value
--- argeweb/libs/wtforms/widgets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .core import *
-
-# Compatibility imports
-from .core import html_params, Input, HTMLString
